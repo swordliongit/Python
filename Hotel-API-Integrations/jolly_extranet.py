121456209d03296b7b54591a5d10aa1ca04b07b5
swordliongit <kilicarslan.business@gmail.com>
2023-10-23 18:10:04 +0300

    extranet start, stop and get requests implemented

--- Hotel-API-Integrations/jolly_extranet.py
+++ Hotel-API-Integrations/jolly_extranet.py
@@ -1,4 +1,5 @@
 import ast
+from datetime import datetime
 import re
 from pyquery import PyQuery as pq
 import requests
@@ -75,7 +76,7 @@ headers4 = {
     "x-requested-with": "XMLHttpRequest",
 }
 
-headers_kontenjan = {
+headers_quota = {
     "authority": "extranet.jollytur.ws",
     "accept": "*/*",
     "accept-language": "en-US,en;q=0.9,tr;q=0.8",
@@ -162,20 +163,22 @@ def jollyextranet_search(session):
     print(response.text)
 
 
-def jollyextranet_kontenjan(session, HotelId, HotelRoomId, Year, MonthId):
+def jollyextranet_Get_Quota(
+    session: requests.Session, HotelId: str, HotelRoomId: str, date: datetime
+):
     payload = {
         "HotelId": HotelId,
         "HotelRoomId": HotelRoomId,
         "MarketId": "1",
         "ScopePath": "%2F",
-        "Year": Year,
-        "MonthId": MonthId,
+        "Year": date.year,
+        "MonthId": date.month,
     }
 
     # Send the POST request
     response = session.post(
         "https://extranet.jollytur.ws/HotelQuota/HotelRoomQuotaDetail",
-        headers=headers_kontenjan,
+        headers=headers_quota,
         data=payload,
     )
 
@@ -231,11 +234,91 @@ def jollyextranet_kontenjan(session, HotelId, HotelRoomId, Year, MonthId):
         print(f"Request failed with status code {response.status_code}")
 
 
+def jollyextranet_Update_Quota(
+    session: requests.Session,
+    HotelRoomId: str,
+    Quota: int,
+    QuotaDate: datetime,
+    Deadline: datetime,
+    SaleStatus: int,
+):
+    """_summary_
+
+    Args:
+        session (requests.Session): _description_
+
+        HotelRoomId (str):
+        >>> 9027 -> Hotel Çamyuva Beach - Standart
+        >>> 38314 -> Alexius Beach Hotel - Standart
+        >>> 38315 -> Alexius Beach Hotel - Family
+
+        Quota (int): _description_
+        QuotaDate (datetime): _description_
+        Deadline (datetime): _description_
+
+        SaleStatus (int):
+        >>> 0 -> Satışa Açık
+        >>> 1 -> Serbest Satış  (Girilen deadline bile olsa satışa açacaktır)
+        >>> 2 -> Satışı Durdur
+        >>> 3 -> Sor Sat
+    """
+    base_id = 0
+    base_date = datetime(2023, 10, 23)
+    if HotelRoomId == "38314":
+        # Base Id and date
+        base_id = 14925582
+    elif HotelRoomId == "38315":
+        base_id = 14925761
+    elif HotelRoomId == "9027":
+        base_id = 14927121
+
+    # Given date
+    given_date = QuotaDate  # Change this to your desired date
+    # Calculate the number of days between the given date and the base date
+    day_difference = (given_date - base_date).days
+    # Calculate the Id value based on the pattern
+    id_value = base_id + day_difference
+    payload = {
+        "HotelRoomQuotaItemDto[0][Id]": id_value,
+        "HotelRoomQuotaItemDto[0][Quota]": Quota,
+        "HotelRoomQuotaItemDto[0][QuotaDate]": QuotaDate.strftime("%d.%m.%Y %H:%M:%S"),
+        "HotelRoomQuotaItemDto[0][Deadline]": Deadline,
+        "HotelRoomQuotaItemDto[0][HotelRoomQuotaSaleStatus]": SaleStatus,
+    }
+
+    # Send the POST request
+    response = session.post(
+        "https://extranet.jollytur.ws/HotelQuota/HotelRoomQuotaItemUpdate",
+        headers=headers_quota,
+        data=payload,
+    )
+
+    if response.status_code == 200:
+        print(response.text)
+    else:
+        print(f"Request failed with status code {response.status_code}")
+
+
 def main():
     s = requests.Session()
     s = jollyextranet_login(s)
-    # jollyextranet_search(s)
-    jollyextranet_kontenjan(s, "14056", "38315", "2023", "10")
+
+    jollyextranet_search(s)
+
+    # Get Rooms
+    jollyextranet_Get_Quota(
+        s, HotelId="14056", HotelRoomId="38315", date=datetime(2023, 10, 1)
+    )
+
+    # Update Quota ( Start, Stop )
+    jollyextranet_Update_Quota(
+        s,
+        HotelRoomId="38315",
+        Quota=5,
+        QuotaDate=datetime(2023, 10, 31),
+        Deadline=0,
+        SaleStatus=0,
+    )
 
 
 if __name__ == "__main__":
--- Hotel-API-Integrations/test.py
+++ Hotel-API-Integrations/test.py
@@ -1,43 +1,59 @@
-import datetime
+# import datetime
 
 
-{
-    "ReservationList": [
-        {
-            "OperatorName": "ZEN TOUR TURIZM A.Ş. ANKARA",
-            "ResType": "Y",
-            "ResConf": "W",
-            "Hotel": "H2S",
-            "HotelName": "H2S Test Hotel",
-            "HotelPaximumID": PAXIMUM_ID,
-            "CinDate": datetime(2023, 9, 2).strftime("%Y.%m.%d"),
-            "CoutDate": datetime(2023, 9, 4).strftime("%Y.%m.%d"),
-            "Day": 2,
-            "RoomCode": "SNG",
-            "RoomName": "SINGLE",
-            "RoomCount": 1,
-            "AccomCode": "STD",
-            "AccomName": "STANDART ODA",
-            "BoardCode": "ALL",
-            "BoardName": "ALL INCLUSIVE (AI)",
-            "Adl": 2,
-            "ExtBed": 0,
-            "Chd": 0,
-            "Inf": 0,
-            "TotalPax": 2,
-            "ResNote": "TEST NOTE",
-            "ResAmount": 100.0,
-            "ResAmountCurr": "TR",
-            "SellingDate": datetime(2023, 9, 2).strftime("%Y.%m.%d"),
-            "AllotmentType": "G",
-            "ResUser": "ARTIN",
-            "OperatorCode": "TUROP",
-            "ResNo": "123abc",
-            "ResOrder": 1,
-            "DatabaseName": "ARTINSYSTEMS",
-            "ChgDate": datetime(2023, 9, 4).strftime("%Y.%m.%d"),
-            "UniqueID": 1001,
-            "UserName": "ARTINSYSTEMS",
-        }
-    ]
-}
+# {
+#     "ReservationList": [
+#         {
+#             "OperatorName": "ZEN TOUR TURIZM A.Ş. ANKARA",
+#             "ResType": "Y",
+#             "ResConf": "W",
+#             "Hotel": "H2S",
+#             "HotelName": "H2S Test Hotel",
+#             "HotelPaximumID": PAXIMUM_ID,
+#             "CinDate": datetime(2023, 9, 2).strftime("%Y.%m.%d"),
+#             "CoutDate": datetime(2023, 9, 4).strftime("%Y.%m.%d"),
+#             "Day": 2,
+#             "RoomCode": "SNG",
+#             "RoomName": "SINGLE",
+#             "RoomCount": 1,
+#             "AccomCode": "STD",
+#             "AccomName": "STANDART ODA",
+#             "BoardCode": "ALL",
+#             "BoardName": "ALL INCLUSIVE (AI)",
+#             "Adl": 2,
+#             "ExtBed": 0,
+#             "Chd": 0,
+#             "Inf": 0,
+#             "TotalPax": 2,
+#             "ResNote": "TEST NOTE",
+#             "ResAmount": 100.0,
+#             "ResAmountCurr": "TR",
+#             "SellingDate": datetime(2023, 9, 2).strftime("%Y.%m.%d"),
+#             "AllotmentType": "G",
+#             "ResUser": "ARTIN",
+#             "OperatorCode": "TUROP",
+#             "ResNo": "123abc",
+#             "ResOrder": 1,
+#             "DatabaseName": "ARTINSYSTEMS",
+#             "ChgDate": datetime(2023, 9, 4).strftime("%Y.%m.%d"),
+#             "UniqueID": 1001,
+#             "UserName": "ARTINSYSTEMS",
+#         }
+#     ]
+# }
+
+
+from datetime import datetime
+
+# Base Id and date
+base_id = 14925582
+base_date = datetime(2023, 10, 31)
+
+# Given date
+given_date = datetime(2023, 10, 31)  # Change this to your desired date
+# Calculate the number of days between the given date and the base date
+day_difference = (given_date - base_date).days
+# Calculate the Id value based on the pattern
+id_value = base_id + day_difference
+
+print(id_value)
